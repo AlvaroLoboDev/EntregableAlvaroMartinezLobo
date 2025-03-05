a6fc79384a318e5f4ad9c0df13abf46d30fc479a
Alvaro Lobo <alvarolobodev@gmail.com>
2025-03-05 23:06:53 +0100

    Ejercicio - Apartado 7

---
+++ js/AlvaroJS.js
@@ -0,0 +1,8 @@
+console.log("Actividad Git - JavaScript");
+
+document.addEventListener("DOMContentLoaded", function() {
+    let mensaje = document.createElement("p");
+    mensaje.textContent = "Este mensaje fue añadido con JavaScript.";
+    mensaje.style.fontWeight = "bold";
+    document.body.appendChild(mensaje);
+});
